287586dd5d40252f096d14955e928cca85b37fce
podobar <robert.podoba73@gmail.com>
2020-11-09 10:16:11 +0100

    Initial commit, main loop to be implemented

---
+++ ARTNetwork.py
@@ -0,0 +1,81 @@
+import numpy as np
+# Parameters description: http://www.inf.ufsc.br/~aldo.vw/patrec/SNNS/UserManual/node206.html#SECTION0010122220000000000000
+class ARTNetwork:
+    Weights_T = list() #Output layer -> M x N -> Input layer
+    Weights_W = list() #Input layer -> N x M -> Output layer
+    Probability = list() #
+    Mask = list()
+    Scaler = list()
+#
+     # 0 <= x <= 1, 0.7 <= x <= 1 suggested
+# Strength of the influence of the lower level in F1 by the middle level
+# Parameter a defines the importance of the expection of F2 , propagated to F1.
+# Affects speed of stabilization in F1
+     # a > 0,  normally a >> 1
+# Strength of the influence of the middle level in F1  by the upper level.
+# For parameter b things are similar to parameter a.  A high value for b is even more important, because otherwise the network could become instable
+    # b > 0, suggested b >> 1
+# Used to compute the error
+     # 0 < c < 1
+#Output value of the F2  winner unit
+    #d = 0.2 # 0 < d < 1
+#Theta - kind of threshold
+     # 0 < theta < 1
+
+    def __init__(self,_input_size: int, _init_output_size:int = 1, _vp:float = 0.9, _a: float = 10, _b: float = 10, _c: float = 0.1, _theta: float = 0.0):
+        self.vigilance_parameter = _vp
+        self.a =_a
+        self.b =_b
+        self.c =_c
+        self.theta =_theta
+        for i in range(_input_size):
+            self.Weights_W.append(list())
+            for j in range(_init_output_size):
+                self.Weights_W[i].append(1/(1+_input_size))
+        for j in range(_init_output_size):
+            self.Weights_T.append(np.ones(_input_size))
+
+    def __insert_new_category(self):
+        n = len(self.Weights_W)
+        for i in range(n):
+            self.Weights_W[i].append(1/(1+n))
+        self.Weights_T.append(np.ones(n))
+
+    def normalize(self, x):
+        return
+
+    def learn(self, data_set, cycles: int):
+        for i in range(cycles):
+            Mask = np.ones(len(self.Weights_T))
+            go_back = True
+            while(max(Mask) > 0 and go_back):
+                best_category_index = self.__choose_best_category(data_set[i % len(data_set)])
+                if self.__compare_input(data_set[i % len(data_set)], self.Weights_T[best_category_index]):
+                    return#delete
+                    #Recognized
+                else:
+                    # Not recognized, try again with another category (if available). If not possible - create new category
+                    continue
+            if max(Mask) == 0:
+                return#delete
+                #Create new category
+        return
+
+    @staticmethod
+    def __compare_input(x, pattern, threshold):
+        return np.divide(np.multiply(x,pattern),np.sum(x)) <= threshold
+
+    def __choose_best_category(self, x):
+        for j in range(len(self.Weights_T)):
+            self.Probability[j] = 0.0
+            for i in range(len(x)):
+                self.Probability[j] += self.Weights_W[i,j] * x[i]
+        return self.Probability.index(max(self.Probability))
+
+    def __update_weights_W(self, x, best_category_index: int):
+        for i in range(len(x)):
+            self.Weights_W[i, best_category_index] = (self.Weights_T[best_category_index,i] * x[i]) / (self.a + np.sum(np.multiply(self.Weights_T[best_category_index], x)))
+
+    def __update_weights_T(self, x, best_category_index: int):
+        for i in range(len(x)):
+            self.Weights_T[best_category_index, i] *= x[i]
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import csv
+import numpy as np
+
+from ARTNetwork import ARTNetwork
+
+
+def load_csv(filename):
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        _data = list(reader)
+
+    for index in range(1, len(_data)):
+        _data[index] = [float(item) for item in _data[index]]
+
+    return _data
+
+
 if __name__ == '__main__':
-    print('init')
+    path='clustering/cube.csv'
+    raw_data = load_csv(path)[1:]
+    classification = list()
+    for i in range(len(raw_data)):
+        classification.append(raw_data[i].pop())
+    network = ARTNetwork(_input_size=len(raw_data[0]), _init_output_size=2)
+    network.learn(data_set=raw_data, cycles=1200)
 
